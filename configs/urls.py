7a35fb604d697816cbe60f24e45541fd32659ce2
des9tka <des9tka2005@ukr.net>
2022-11-27 19:05:27 +0200

    comm

--- configs/urls.py
+++ configs/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from users.views import UsersReadWriteView
+from users.views import UsersReadWriteView, UserGetUpdateDeleteView
 
 urlpatterns = [
-    path('users', UsersReadWriteView.as_view())
+    path('users', UsersReadWriteView.as_view()),
+    path('users/<int:pk>', UserGetUpdateDeleteView.as_view())
 ]
--- users/views.py
+++ users/views.py
@@ -3,8 +3,6 @@ import json
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-import users
-
 
 class FileReadWriteService:
     _file_name = None
@@ -13,13 +11,12 @@ class FileReadWriteService:
     def get_users(cls):
         try:
             with open(cls._file_name) as file:
-                print(json.load(file))
                 return json.load(file)
         except (Exception,):
             return []
 
     @classmethod
-    def add_user(cls, data):
+    def save_users(cls, data):
         try:
             with open(cls._file_name, mode='w') as file:
                 json.dump(data, file)
@@ -37,20 +34,48 @@ class CustomAPIView(APIView, FileReadWriteService):
 
 
 class UsersReadWriteView(CustomAPIView):
+
     def get(self, *args, **kwargs):
         return Response(self.users)
 
-    def put(self, *args, **kwargs):
+    def post(self, *args, **kwargs):
         data = self.request.data
-        data['id'] = self.users[-1]['id']+1 if self.users else 1
-        self.add_user(self.users)
+        data['id'] = self.users[-1]['id'] + 1 if self.users else 1
+        self.users.append(data)
+        self.save_users(self.users)
         return Response(data)
 
 
+class UserGetUpdateDeleteView(CustomAPIView):
 
+    def _get_user_by_id(self, pk):
+        try:
+            user = next((user for user in self.users if user['id'] == pk))
+            return user
+        except (Exception,):
+            return 'Not found'
 
+    def get(self, *args, **kwargs):
+        pk = kwargs.get('pk')
+        return Response(self._get_user_by_id(pk))
 
+    def put(self, *args, **kwargs):
+        data = self.request.data
+        pk = kwargs.get('pk')
+        user = self._get_user_by_id(pk)
+        if data.get('id'):
+            del data['id']
+        user |= data
+        self.save_users(self.users)
+        return Response(data)
 
+    def delete(self, *args, **kwargs):
+        pk = kwargs.get('pk')
+        index = next((i for i, v in enumerate(self.users) if v['id'] == pk), None)
 
+        if index is None:
+            return Response('Not Found')
 
-
+        del self.users[index]
+        self.save_users(self.users)
+        return Response('deleted')
