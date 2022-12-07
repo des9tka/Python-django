13346d0a3f3e571b0efcd7ea564b8b2500c7a1ec
des9tka <des9tka2005@ukr.net>
2022-12-07 20:53:52 +0200

    hm4

---
+++ apps/users/serializers.py
@@ -0,0 +1,32 @@
+from typing import Type
+
+from django.contrib.auth import get_user_model
+
+from rest_framework.serializers import ModelSerializer
+
+from apps.users.models import UserModel as User
+
+UserModel: Type[User] = get_user_model()
+
+
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = UserModel
+        fields = (
+            'id', 'email', 'password', 'is_staff', 'is_active', 'is_superuser', 'created_at', 'update_at', 'last_login'
+        )
+        read_only_fields = ('id', 'is_staff', 'is_active', 'is_superuser', 'created_at', 'update_at', 'last_login')
+        extra_kwargs = {
+            'password': {
+                'write_only': True
+            }
+        }
+
+    def create(self, validated_data):
+        user = UserModel.objects.create_user(validated_data)
+        return user
+
+
+
+
--- configs/urls.py
+++ configs/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path, include
+from django.urls import include, path
+
+import apps.users.urls
 
 urlpatterns = [
+    path('user', include(('apps.users.urls'))),
     path('cars', include('apps.cars.urls')),
     path('auto_parks', include('apps.auto_parks.urls'))
 ]
