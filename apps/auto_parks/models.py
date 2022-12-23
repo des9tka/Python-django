e782c6a66559ac6349c65d6d939da29d64cb5883
des9tka <des9tka2005@ukr.net>
2022-12-23 19:38:35 +0200

    hm 8

--- apps/auto_parks/models.py
+++ apps/auto_parks/models.py
@@ -8,4 +8,12 @@ class AutoParksModel(models.Model):
         db_table = 'auto_parks'
 
     name = models.CharField(max_length=20)
-    user = models.ForeignKey(UserModel, on_delete=models.CASCADE, related_name='auto_parks')
+    users = models.ManyToManyField(UserModel, through='UsersAutoParksModel', related_name='auto_parks')
+
+
+class UsersAutoParksModel(models.Model):
+    class Meta:
+        db_table = 'cars_auto_parks'
+
+    user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    auto_parks = models.ForeignKey(AutoParksModel, on_delete=models.CASCADE)
